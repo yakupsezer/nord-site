bb9068eae0594acd2425bbee43fd455c02fe3dc2
yakupsezer <ys@pionr.ventures>
2026-07-30 15:02:57 +0300

    Updated gizlilik.html

--- nord-shared.js
+++ nord-shared.js
@@ -91,11 +91,16 @@ function Logo({
   size = '',
   href = null
 }) {
-  const inner = /*#__PURE__*/React.createElement("img", {
-    src: `${BASE}assets/nord-logo-tescil-05.png`,
+  const inner = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("img", {
+    src: `${BASE}assets/nord-logo-white.png`,
     alt: "Nord",
-    className: "logo-img"
-  });
+    className: "logo-img on-dark"
+  }), /*#__PURE__*/React.createElement("img", {
+    src: `${BASE}assets/nord-logo-black.png`,
+    alt: "",
+    "aria-hidden": "true",
+    className: "logo-img on-light"
+  }));
   const cls = `logo ${size}`;
   return href !== null ? /*#__PURE__*/React.createElement("a", {
     href: href,
